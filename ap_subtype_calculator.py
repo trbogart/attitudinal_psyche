77bf52bf4a642e7809b6edc51c900f84a9ef820f
Richard Bogart <trbogart@gmail.com>
2025-05-18 10:52:20 -0700

    rename

--- ap_subtype_calculator.py
+++ ap_subtype_calculator.py
@@ -1,30 +1,40 @@
 import sys
+import argparse
 
-def validate_type(s):
-    return len(s) == 4 and sorted(s) == ['E', 'F', 'L', 'V']
-
-def validate_subtype(s):
-    return len(s) == 4 and all(map(lambda c: '0' <= c <= '4', s))
-
-def input_type():
+def input_ap_type():
     while True:
-        type_str = input('Enter AP type (q to quit): ').upper()
-        if type_str == 'Q':
+        ap_type_str = input('Enter AP type (q to quit): ')
+        if ap_type_str in ['q', 'Q']:
             sys.exit(0)
-        if validate_type(type_str):
-            return list(type_str)
+        ap_type = validate_and_parse_ap_type(ap_type_str)
+        if ap_type:
+            return ap_type
         else:
-            print('Invalid type')
+            print('Invalid subtype')
+
+def validate_and_parse_ap_type(type_str):
+    type_str = type_str.upper()
+    if len(type_str) == 4 and sorted(type_str) == ['E', 'F', 'L', 'V']:
+        return list(type_str)
+    else:
+        return None
 
 def input_subtype():
     while True:
         subtype_str = input('Enter AP subtype (q to quit): ')
-        if subtype_str.upper() == 'Q':
+        if subtype_str in ['q', 'Q']:
             exit(0)
-        if validate_subtype(subtype_str):
-            return { i+1: int(c) for i, c in enumerate(subtype_str)}
+        subtype = validate_and_parse_subtype(subtype_str)
+        if subtype:
+            return subtype
         else:
             print('Invalid subtype')
+
+def validate_and_parse_subtype(subtype_str):
+    if len(subtype_str) == 4 and all(map(lambda c: '0' <= c <= '4', subtype_str)):
+        return {i + 1: int(c) for i, c in enumerate(subtype_str)}
+    else:
+        return None
 
 def swap_positions(shadow_types, ap_type, pos1, pos2):
     ap_type[pos1 - 1], ap_type[pos2 - 1] = ap_type[pos2 - 1], ap_type[pos1 - 1]
@@ -70,12 +80,41 @@ def get_shadow_types(ap_type, subtype):
 
     return shadow_types
 
+
+def print_shadow_types(ap_type, subtype):
+    original_ap_type = ''.join(ap_type)
+    subtype_str = ''.join(map(str, subtype.values()))
+    shadow_types = get_shadow_types(ap_type, subtype)
+    print(f'Shadow types for {original_ap_type} {subtype_str}:')
+    for shadow_type in shadow_types:
+        print(shadow_type)
+
 if __name__ == '__main__':
-    while True:
-        ap_type = input_type()
-        subtype = input_subtype()
-        shadow_types = get_shadow_types(ap_type, subtype)
-        print('Shadow types:')
-        for shadow_type in shadow_types:
-            print(shadow_type)
-        print()
+    if len(sys.argv) == 1:
+        # interactive mode
+        while True:
+            ap_type = input_ap_type()
+            subtype = input_subtype()
+            print_shadow_types(ap_type, subtype)
+            print()
+    else:
+        parser = argparse.ArgumentParser(
+            'ap_shadow_type_calculator',
+            'Calculate AP shadow types (no arguments to run interactively)',
+        )
+        parser.add_argument('ap_type')
+        parser.add_argument('subtype')
+        args = parser.parse_args()
+
+        # at least 1 argument given, get missing argument, if any
+        ap_type = validate_and_parse_ap_type(args.ap_type)
+        if not ap_type:
+            sys.stderr.write(f'Invalid AP type: {args.ap_type}\n')
+            exit(1)
+
+        subtype = validate_and_parse_subtype(args.subtype)
+        if not subtype:
+            sys.stderr.write(f'Invalid subtype: {args.subtype}\n')
+            exit(1)
+
+        print_shadow_types(ap_type, subtype)
