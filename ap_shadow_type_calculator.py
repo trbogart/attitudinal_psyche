54c8d2af3bd2442fda8e9e071f7e41ef78cf4144
Richard Bogart <trbogart@gmail.com>
2025-05-23 08:39:03 -0700

    refactor, formatting, add tests

--- ap_shadow_type_calculator.py
+++ ap_shadow_type_calculator.py
@@ -9,128 +9,133 @@
 import argparse
 import sys
 
+class ShadowTypes:
+    def __init__(self, ap_type_str: str, subtype_str: str):
+        validate_ap_type(ap_type_str)
+        validate_subtype(subtype_str)
 
-def input_ap_type():
+        self.ap_type_str = ap_type_str.upper()
+        self.shadow_type = list(self.ap_type_str)
+
+        self.subtype_str = subtype_str
+        self.subtype = {i + 1: int(c) for i, c in enumerate(subtype_str)}
+
+        self.steps = []
+        self.shadow_types = [self.ap_type_str]
+        self.swapped_pairs = set()
+
+        # Step 1 - Accentuated - Ignore
+
+        # Step 2 - Obscured - Any aspect pointing at an obscured position (x-0) switches
+        # with it, unless it has another aspect pointing at it
+        for pos, pos_subtype in self.subtype.items():
+            if pos_subtype == 0:  # obscured
+                self.swap_obscured_shadow_type(pos)
+
+        # Step 3 - Method - Switch aspects in this order: 2-3 or 3-2 > 1-4 or 4-1
+        self.swap_shadow_type('Method 2-3', 2, 3)
+        self.swap_shadow_type('Method 1-4', 1, 4)
+
+        # Step 4 - Self - Switch aspects in this order: 1-2 or 2-1 > 3-4 or 4-3
+        self.swap_shadow_type('Self 1-2', 1, 2)
+        self.swap_shadow_type('Self 3-4', 3, 4)
+
+        # Step 5 - Others - Switch aspects in this order: 1-3 or 3-1 > 2-4 or 4-2
+        self.swap_shadow_type('Others 1-3', 1, 3)
+        self.swap_shadow_type('Others 2-4', 2, 4)
+
+
+    def description(self):
+        return f'{self.ap_type_str} {self.subtype_str}'
+
+    def swap_positions(self, description: str, pos1: int, pos2: int):
+        if pos1 > pos2:
+            pair = f'{pos2}-{pos1}'
+        else:
+            pair = f'{pos1}-{pos2}'
+        if pair in self.swapped_pairs:
+            self.steps.append(f'Skip {description} - already swapped')
+            return
+
+        self.swapped_pairs.add(pair)
+
+        self.shadow_type[pos1 - 1], self.shadow_type[pos2 - 1] = self.shadow_type[pos2 - 1], self.shadow_type[pos1 - 1]
+        shadow_type_str = ''.join(self.shadow_type)
+        self.steps.append(f'Swap {description} -> {shadow_type_str}')
+        self.shadow_types.append(shadow_type_str)
+
+    def swap_obscured_shadow_type(self, pos: int):
+        swap_pos = 0
+        for other_pos, pos_subtype in self.subtype.items():
+            if pos_subtype == pos:
+                if swap_pos > 0:
+                    # multiple matches, do not swap
+                    self.steps.append(f'Skip Obscured {pos} - multiple matches')
+                    return
+                swap_pos = other_pos
+        if swap_pos > 0:
+            self.swap_positions(f'Obscured {pos}-{swap_pos}', pos, swap_pos)
+        else:
+            self.steps.append(f'Skip Obscured {pos} - no matches')
+
+    def swap_shadow_type(self, description: str, pos1: int, pos2: int):
+        if self.subtype[pos1] == pos2 or self.subtype[pos2] == pos1:
+            self.swap_positions(description, pos1, pos2)
+        else:
+            self.steps.append(f'Skip {description}')
+
+def input_ap_type() -> str:
     while True:
         ap_type_str = input('Enter AP type (q to quit): ')
         if ap_type_str in {'q', 'Q'}:
             sys.exit(0)
         try:
-            return parse_ap_type(ap_type_str)
+            validate_ap_type(ap_type_str)
+            return ap_type_str
         except ValueError as e:
             print(f'{e.args[0]}')
 
-
-def input_subtype():
+def input_subtype() -> str:
     while True:
         subtype_str = input('Enter AP subtype (q to quit): ')
         if subtype_str in {'q', 'Q'}:
             exit(0)
         try:
-            return parse_subtype(subtype_str)
+            validate_subtype(subtype_str)
+            return subtype_str
         except ValueError as e:
             print(f'{e.args[0]}')
 
-
-def swap_positions(description, shadow_types, swapped, ap_type, pos1, pos2):
-    if pos1 > pos2:
-        pair = f'{pos2}-{pos1}'
-    else:
-        pair = f'{pos1}-{pos2}'
-    if pair in swapped:
-        print(f'Skip {description} - already swapped')
-        return
-
-    swapped.add(pair)
-
-    ap_type[pos1 - 1], ap_type[pos2 - 1] = ap_type[pos2 - 1], ap_type[pos1 - 1]
-    shadow_type = ''.join(ap_type)
-    print(f'Swap {description}: {shadow_type}')
-    shadow_types.append(shadow_type)
-
-
-def swap_obscured_shadow_type(ap_type, shadow_types, swapped, subtype, pos):
-    swap_pos = 0
-    for other_pos, pos_subtype in subtype.items():
-        if pos_subtype == pos:
-            if swap_pos > 0:
-                # multiple matches, do not swap
-                print(f'Skip Obscured {pos} - multiple matches')
-                return
-            swap_pos = other_pos
-    if swap_pos > 0:
-        swap_positions(f'Obscured {pos}-{swap_pos}', shadow_types, swapped, ap_type, pos, swap_pos)
-    else:
-        print(f'Skip Obscured {pos} - no matches')
-
-
-def swap_shadow_type(description, shadow_types, swapped, ap_type, subtype, pos1, pos2):
-    if subtype[pos1] == pos2 or subtype[pos2] == pos1:
-        swap_positions(description, shadow_types, swapped, ap_type, pos1, pos2)
-    else:
-        print(f'Skip {description}')
-
-def print_shadow_types(ap_type, subtype):
-    subtype_str = ''.join(map(str, subtype.values()))
-    original_ap_type = ''.join(ap_type)
-    shadow_types = [original_ap_type]
-    swapped = set()
-    print(f'Shadow types for {original_ap_type} {subtype_str}:')
-
-    # Step 1 - Accentuated - Ignore
-
-    # Step 2 - Obscured - Any aspect pointing at an obscured position (x-0) switches
-    # with it, unless it has another aspect pointing at it
-    for pos, pos_subtype in subtype.items():
-        if pos_subtype == 0: # obscured
-            swap_obscured_shadow_type(ap_type, shadow_types, swapped, subtype, pos)
-
-    # Step 3 - Method - Switch aspects in this order: 2-3 or 3-2 > 1-4 or 4-1
-    swap_shadow_type('Method 2-3', shadow_types, swapped, ap_type, subtype, 2, 3)
-    swap_shadow_type('Method 1-4', shadow_types, swapped, ap_type, subtype, 1, 4)
-
-    # Step 4 - Self - Switch aspects in this order: 1-2 or 2-1 > 3-4 or 4-3
-    swap_shadow_type('Self 1-2', shadow_types, swapped, ap_type, subtype, 1, 2)
-    swap_shadow_type('Self 3-4', shadow_types, swapped, ap_type, subtype, 3, 4)
-
-    # Step 5 - Others - Switch aspects in this order: 1-3 or 3-1 > 2-4 or 4-2
-    swap_shadow_type('Others 1-3', shadow_types, swapped, ap_type, subtype, 1, 3)
-    swap_shadow_type('Others 2-4', shadow_types, swapped, ap_type, subtype, 2, 4)
-
-    print(f'Shadow types: {", ".join(shadow_types)}')
-
-
-def parse_ap_type(ap_type_str):
-    ap_type_str = ap_type_str.upper()
-    if len(ap_type_str) == 4 and sorted(ap_type_str) == ['E', 'F', 'L', 'V']:
-        return list(ap_type_str)
-    else:
+def validate_ap_type(ap_type_str: str) -> None:
+    if len(ap_type_str) != 4 or sorted(ap_type_str.upper()) != ['E', 'F', 'L', 'V']:
         raise ValueError(f'Invalid AP type {ap_type_str}')
 
-
-def parse_subtype(subtype_str):
-    if len(subtype_str) == 4 and all(map(lambda c: '0' <= c <= '4', subtype_str)):
-        return {i + 1: int(c) for i, c in enumerate(subtype_str)}
-    else:
+def validate_subtype(subtype_str: str) -> None:
+    if len(subtype_str) != 4 or not all(map(lambda c: '0' <= c <= '4', subtype_str)):
         raise ValueError(f'Invalid subtype {subtype_str}')
 
+def print_shadow_types(ap_type_str: str, subtype_str: str):
+    shadow_types = ShadowTypes(ap_type_str, subtype_str)
+    print(f'Steps:')
+    for step in shadow_types.steps:
+        print(f'- {step}')
+    print()
+    print(f'Shadow types for {shadow_types.description()}')
+    for shadow_type in shadow_types.shadow_types:
+        print(f'- {shadow_type}')
 
 def run_interactive():
-    ap_type = input_ap_type()
-    subtype = input_subtype()
-    print_shadow_types(ap_type, subtype)
+    ap_type_str = input_ap_type()
+    subtype_str = input_subtype()
+    print_shadow_types(ap_type_str, subtype_str)
     print()
 
-
-def run_with_args(ap_type_str, subtype_str):
+def run_with_args(ap_type_str: str, subtype_str: str):
     try:
-        ap_type = parse_ap_type(ap_type_str)
-        subtype = parse_subtype(subtype_str)
-        print_shadow_types(ap_type, subtype)
+        print_shadow_types(ap_type_str, subtype_str)
     except ValueError as e:
         sys.stderr.write(f'{e.args[0]}\n')
         exit(1)
-
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
@@ -146,5 +151,3 @@ if __name__ == '__main__':
         parser.add_argument('subtype')
         args = parser.parse_args()
         run_with_args(args.ap_type, args.subtype)
-
-
---
+++ test/ap_shadow_type_calculator_test.py
@@ -0,0 +1,91 @@
+import unittest
+from itertools import permutations
+
+from ap_shadow_type_calculator import validate_subtype, validate_ap_type, ShadowTypes
+
+class ApShadowTypeCalculatorTest(unittest.TestCase):
+
+    def verify_shadow_types(self, ap_type, subtype, *expected_shadow_types):
+        shadow_types = ShadowTypes(ap_type, subtype).shadow_types
+        self.assertListEqual(list(expected_shadow_types), shadow_types)
+
+    def test_validate_ap_type_invalid(self):
+        with self.assertRaises(ValueError):
+            validate_ap_type('ABCD')
+        with self.assertRaises(ValueError):
+            validate_ap_type('VLLE')
+        with self.assertRaises(ValueError):
+            validate_ap_type('VLEFA')
+        with self.assertRaises(ValueError):
+            validate_ap_type('VLE')
+
+    def test_validate_ap_type_valid(self):
+        for ap_type in permutations(['V', 'L', 'E', 'F']):
+            validate_ap_type(''.join(ap_type))
+
+    def test_validate_subtype_invalid(self):
+        with self.assertRaises(ValueError):
+            validate_ap_type('ABCD')
+        with self.assertRaises(ValueError):
+            validate_ap_type('1005')
+        with self.assertRaises(ValueError):
+            validate_ap_type('100')
+        with self.assertRaises(ValueError):
+            validate_ap_type('10041')
+
+    def test_validate_subtype_valid(self):
+        for pos1 in range(5):
+            for pos2 in range(5):
+                for pos3 in range(5):
+                    for pos4 in range(5):
+                        subtype = str(pos1) + str(pos2) + str(pos3) + str(pos4)
+                        validate_subtype(subtype)
+
+    def test_all_obscured(self):
+        self.verify_shadow_types('velf', '0000', 'VELF')
+
+    def test_all_accentuated(self):
+        self.verify_shadow_types('velf', '1234', 'VELF')
+
+    def test_accentuated_and_obscured(self):
+        self.verify_shadow_types('velf', '1200', 'VELF')
+
+    def test_swap_obscured(self):
+        self.verify_shadow_types('VFEL', '1340', 'VFEL', 'VFLE', 'VLFE')
+
+    def test_swap_nultiple_obscured(self):
+        self.verify_shadow_types('vlef', '4300', 'VLEF', 'VELF', 'FELV')
+
+    def test_skip_swap_obscured_multiple_matches(self):
+        self.verify_shadow_types('FEVL', '1440', 'FEVL', 'FELV', 'FVLE')
+
+    def test_swap_method_2(self):
+        self.verify_shadow_types('LEVF', '1224', 'LEVF', 'LVEF')
+
+    def test_swap_method_3(self):
+        self.verify_shadow_types('LEVF', '1334', 'LEVF', 'LVEF')
+
+    def test_swap_method_2_3(self):
+        self.verify_shadow_types('LEVF', '1324', 'LEVF', 'LVEF')
+
+    def test_swap_method_1(self):
+        self.verify_shadow_types('FLVE', '4234', 'FLVE', 'ELVF')
+
+    def test_swap_method_4(self):
+        self.verify_shadow_types('FLVE', '1231', 'FLVE', 'ELVF')
+
+    def test_swap_method_1_and_4(self):
+        self.verify_shadow_types('FLVE', '4231', 'FLVE', 'ELVF')
+
+    def test_dual_shadow_1(self):
+        self.verify_shadow_types('LFEV', '1221', 'LFEV', 'LEFV', 'VEFL')
+
+    def test_dual_shadow_2(self):
+        self.verify_shadow_types('LFEV', '4334', 'LFEV', 'LEFV', 'VEFL')
+
+    def test_conflictor_shadow(self):
+        self.verify_shadow_types('evlf', '4124', 'EVLF', 'ELVF', 'FLVE', 'LFVE')
+
+
+if __name__ == '__main__':
+    unittest.main()
