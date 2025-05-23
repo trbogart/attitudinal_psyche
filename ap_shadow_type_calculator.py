d029980357c8f716fb3994fd84057cf1f2a7265a
Richard Bogart <trbogart@gmail.com>
2025-05-23 16:33:50 -0700

    fix aspects, refactor

--- ap_shadow_type_calculator.py
+++ ap_shadow_type_calculator.py
@@ -9,76 +9,129 @@
 import argparse
 import sys
 
+verbose = False
+
+class SubType:
+    def __init__(self, ap_type, source_pos, target_pos):
+        self.source_pos = source_pos
+        self.target_pos = target_pos
+        self.aspect = ap_type[source_pos - 1]
+        if source_pos == target_pos:
+            self.subtype_type = 'A'
+            self.type_description = 'Accentuated'
+        elif target_pos == 0:
+            self.subtype_type = 'O' # obscured
+            self.type_description = 'Obscured'
+        elif self._is_subtype_type(2, 3):
+            self.subtype_type = 'M1' # method priority 1 (2-3 or 3-2)
+            self.type_description = 'Method'
+        elif self._is_subtype_type(1, 4):
+            self.subtype_type = 'M2' # method priority 2 (1-4 or 4-1)
+            self.type_description = 'Method'
+        elif self._is_subtype_type(1, 2):
+            self.subtype_type = 'S1'  # self priority 1 (1-2 or 2-1)
+            self.type_description = 'Self'
+        elif self._is_subtype_type(3, 4):
+            self.subtype_type = 'S2'  # self priority 2 (3-4 or 4-3)
+            self.type_description = 'Self'
+        elif self._is_subtype_type(1, 3):
+            self.subtype_type = 'O1'  # self priority 1 (1-3 or 3-1)
+            self.type_description = 'Others'
+        elif self._is_subtype_type(2, 4):
+            self.subtype_type = 'O2'  # self priority 2 (2-4 or 4-2)
+            self.type_description = 'Others'
+        else:
+            raise ValueError(f'Invalid subtype: {self}')
+
+    def __repr__(self):
+        return f'{self.source_pos}{self.aspect}-{self.target_pos} ({self.type_description})'
+
+    def __hash__(self):
+        return hash((self.source_pos, self.target_pos))
+
+    def __eq__(self, other):
+        return self.source_pos == other.source_pos and self.target_pos == other.target_pos
+
+    def _is_subtype_type(self, pos1, pos2):
+        return (self.source_pos == pos1 and self.target_pos == pos2 or
+                self.source_pos == pos2 and self.target_pos == pos1)
+
 class ShadowTypes:
     def __init__(self, ap_type_str: str, subtype_str: str):
         validate_ap_type(ap_type_str)
         validate_subtype(subtype_str)
 
         self.ap_type_str = ap_type_str.upper()
-        self.shadow_type = list(self.ap_type_str)
+        self.last_shadow_type_str = list(self.ap_type_str) # will be mutated to latest shadow type
+        self.original_ap_type = self.last_shadow_type_str[:]
 
         self.subtype_str = subtype_str
-        self.subtype = {i + 1: int(c) for i, c in enumerate(subtype_str)}
+        self.subtypes = [SubType(self.last_shadow_type_str, i + 1, int(c)) for i, c in enumerate(subtype_str)]
 
-        self.steps = []
         self.shadow_types = {self.ap_type_str: "AP type"}
-        self.swapped_to_obfuscated = {}
+        self.swapped_to_obscured = set() # set of SubTypes that have already been swapped
 
-        # Step 1 - Accentuated - Ignore
-
-        # Step 2 - Obscured - Any aspect pointing at an obscured position (x-0) switches
+        # Obscured - Any aspect pointing at an obscured position (x-0) switches
         # with it, unless it has another aspect pointing at it
-        for pos, pos_subtype in self.subtype.items():
-            if pos_subtype == 0:  # obscured
-                self.swap_obscured_shadow_type(pos)
+        for subtype in self.subtypes:
+            if subtype.subtype_type == 'O':
+                self.swap_obscured_shadow_type(subtype)
 
-        # Step 3 - Method - Switch aspects in this order: 2-3 or 3-2 > 1-4 or 4-1
-        self.swap_shadow_type('Method (2/3)', 2, 3)
-        self.swap_shadow_type('Method (1/4)', 1, 4)
+        self.swap_shadow_type('M1') # Method 2-3 or 3-2
+        self.swap_shadow_type('M2') # Method 1-4 or 4-1
 
-        # Step 4 - Self - Switch aspects in this order: 1-2 or 2-1 > 3-4 or 4-3
-        self.swap_shadow_type('Self (1/2)', 1, 2)
-        self.swap_shadow_type('Self (3/4)', 3, 4)
+        self.swap_shadow_type('S1') # Self 1-2 or 2-1
+        self.swap_shadow_type('S2') # Self 3-4 or 4-3
 
-        # Step 5 - Others - Switch aspects in this order: 1-3 or 3-1 > 2-4 or 4-2
-        self.swap_shadow_type('Others (1/3)', 1, 3)
-        self.swap_shadow_type('Others (2/4)', 2, 4)
+        self.swap_shadow_type('O1') # Other 1-3 or 3-1
+        self.swap_shadow_type('O2') # Other 2-4 or 4-2
 
-
-    def description(self):
+    def description(self) -> str:
         return f'{self.ap_type_str} {self.subtype_str}'
 
-    def swap_positions(self, description: str, pos1: int, pos2: int):
-        if self.swapped_to_obfuscated.get(pos1, 0) == pos2 or self.swapped_to_obfuscated.get(pos2, 0) == pos1:
-            self.steps.append(f'Skipped {description} - already swapped')
+    def swap(self, subtype: SubType, obscured_subtype: SubType = None) -> None:
+        if subtype in self.swapped_to_obscured:
+            debug(f'Skipped {subtype} - already swapped')
             return
 
-        self.shadow_type[pos1 - 1], self.shadow_type[pos2 - 1] = self.shadow_type[pos2 - 1], self.shadow_type[pos1 - 1]
-        shadow_type_str = ''.join(self.shadow_type)
-        reason = f'Swapped {description}'
-        self.steps.append(f'{reason} -> {shadow_type_str}')
-        self.shadow_types[shadow_type_str] = reason
+        pos1 = self.last_shadow_type_str.index(subtype.aspect) + 1
+        pos2 = subtype.target_pos
 
-    def swap_obscured_shadow_type(self, pos: int):
-        swap_pos = 0
-        for other_pos, pos_subtype in self.subtype.items():
-            if pos_subtype == pos:
-                if swap_pos > 0:
+        if pos1 == pos2:
+            shadow_type_str = ''.join(self.last_shadow_type_str)
+            # this can happen for subtypes in the same pair, e.g. 2-3 and 3-2
+            debug(f'Already swapped {subtype}')
+            self.shadow_types[shadow_type_str] = f'{self.shadow_types[shadow_type_str]} and {subtype}'
+        else:
+            self.last_shadow_type_str[pos1 - 1], self.last_shadow_type_str[pos2 - 1] = self.last_shadow_type_str[pos2 - 1], self.last_shadow_type_str[pos1 - 1]
+            shadow_type_str = ''.join(self.last_shadow_type_str)
+            debug(f'Swapped {subtype} -> {shadow_type_str}')
+            if obscured_subtype:
+                self.shadow_types[shadow_type_str] = f'Swapped {subtype} for {obscured_subtype}'
+            else:
+                self.shadow_types[shadow_type_str] = f'Swapped {subtype}'
+
+        # else subtypes in the same pair, e.g. 2-3 or 3-2, will usually produce the same result, ignore
+
+    def swap_obscured_shadow_type(self, obscured_subtype: SubType) -> None:
+        swap_subtype = None
+        for other_subtype in self.subtypes:
+            if other_subtype.target_pos == obscured_subtype.source_pos: # obscured
+                if swap_subtype:
                     # multiple matches, do not swap
-                    self.steps.append(f'Skipped Obscured {pos} - multiple matches')
+                    debug(f'Skipped {obscured_subtype} - multiple matches')
                     return
-                swap_pos = other_pos
-        if swap_pos > 0:
-            self.swap_positions(f'Obscured {pos}-{swap_pos}', pos, swap_pos)
-            self.swapped_to_obfuscated[swap_pos] = pos
+                swap_subtype = other_subtype
+        if swap_subtype:
+            self.swap(swap_subtype, obscured_subtype)
+            self.swapped_to_obscured.add(swap_subtype)
         else:
-            self.steps.append(f'Skipped Obscured {pos} - no matches')
+            debug(f'Skipped {obscured_subtype} - no matches')
 
-    def swap_shadow_type(self, description: str, pos1: int, pos2: int):
-        if self.subtype[pos1] == pos2 or self.subtype[pos2] == pos1:
-            self.swap_positions(description, pos1, pos2)
-        else:
-            self.steps.append(f'Skip {description}')
+    def swap_shadow_type(self, subtype_type: str):
+        for subtype in self.subtypes:
+            if subtype.subtype_type == subtype_type:
+                self.swap(subtype)
 
 def input_ap_type() -> str:
     while True:
@@ -110,29 +163,28 @@ def validate_subtype(subtype_str: str) -> None:
     if len(subtype_str) != 4 or not all(map(lambda c: '0' <= c <= '4', subtype_str)):
         raise ValueError(f'Invalid subtype {subtype_str}')
 
-def print_shadow_types(ap_type_str: str, subtype_str: str):
+def print_shadow_types(ap_type_str: str, subtype_str: str) -> None:
     shadow_types = ShadowTypes(ap_type_str, subtype_str)
     print(f'Shadow types for {shadow_types.description()}')
     for shadow_type, reason in shadow_types.shadow_types.items():
         print(f'- {shadow_type}: {reason}')
 
-    # print()
-    # print(f'Steps:')
-    # for step in shadow_types.steps:
-    #     print(f'- {step}')
-
-def run_interactive():
+def run_interactive() -> None:
     ap_type_str = input_ap_type()
     subtype_str = input_subtype()
     print_shadow_types(ap_type_str, subtype_str)
     print()
 
-def run_with_args(ap_type_str: str, subtype_str: str):
+def run_with_args(ap_type_str: str, subtype_str: str) -> None:
     try:
         print_shadow_types(ap_type_str, subtype_str)
     except ValueError as e:
         sys.stderr.write(f'{e.args[0]}\n')
         exit(1)
+
+def debug(s) -> None:
+    if verbose:
+        print(s)
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
--- test/ap_shadow_type_calculator_test.py
+++ test/ap_shadow_type_calculator_test.py
@@ -45,13 +45,13 @@ class ApShadowTypeCalculatorTest(unittest.TestCase):
         self.verify_shadow_types('velf', '0000', 'VELF')
 
     def test_all_accentuated(self):
-        self.verify_shadow_types('velf', '1234', 'VELF')
+        self.verify_shadow_types('lvef', '1234', 'LVEF')
 
     def test_accentuated_and_obscured(self):
         self.verify_shadow_types('velf', '1200', 'VELF')
 
     def test_swap_obscured(self):
-        self.verify_shadow_types('VFEL', '1340', 'VFEL', 'VFLE', 'VLFE')
+        self.verify_shadow_types('VFEL', '1240', 'VFEL', 'VFLE')
 
     def test_swap_multiple_obscured(self):
         self.verify_shadow_types('vlef', '4300', 'VLEF', 'VELF', 'FELV')
@@ -59,23 +59,35 @@ class ApShadowTypeCalculatorTest(unittest.TestCase):
     def test_skip_swap_obscured_multiple_matches(self):
         self.verify_shadow_types('FEVL', '1440', 'FEVL', 'FELV', 'FVLE')
 
-    def test_swap_method_2(self):
+    def test_swap_method1(self):
         self.verify_shadow_types('LEVF', '1224', 'LEVF', 'LVEF')
-
-    def test_swap_method_3(self):
         self.verify_shadow_types('LEVF', '1334', 'LEVF', 'LVEF')
-
-    def test_swap_method_2_3(self):
         self.verify_shadow_types('LEVF', '1324', 'LEVF', 'LVEF')
 
-    def test_swap_method_1(self):
+    def test_swap_method2(self):
         self.verify_shadow_types('FLVE', '4234', 'FLVE', 'ELVF')
-
-    def test_swap_method_4(self):
         self.verify_shadow_types('FLVE', '1231', 'FLVE', 'ELVF')
-
-    def test_swap_method_1_and_4(self):
         self.verify_shadow_types('FLVE', '4231', 'FLVE', 'ELVF')
+
+    def test_swap_self1(self):
+        self.verify_shadow_types('LEFV', '2200', 'LEFV', 'ELFV')
+        self.verify_shadow_types('LEFV', '1100', 'LEFV', 'ELFV')
+        self.verify_shadow_types('LEFV', '2100', 'LEFV', 'ELFV')
+
+    def test_swap_self2(self):
+        self.verify_shadow_types('LEFV', '1240', 'LEFV', 'LEVF')
+        self.verify_shadow_types('LEFV', '1203', 'LEFV', 'LEVF')
+        self.verify_shadow_types('LEFV', '1243', 'LEFV', 'LEVF')
+
+    def test_swap_other1(self):
+        self.verify_shadow_types('LVFE', '3200', 'LVFE', 'FVLE')
+        self.verify_shadow_types('LVFE', '1210', 'LVFE', 'FVLE')
+        self.verify_shadow_types('LVFE', '3210', 'LVFE', 'FVLE')
+
+    def test_swap_other2(self):
+        self.verify_shadow_types('LVFE', '1400', 'LVFE', 'LEFV')
+        self.verify_shadow_types('LVFE', '1002', 'LVFE', 'LEFV')
+        self.verify_shadow_types('LVFE', '1402', 'LVFE', 'LEFV')
 
     def test_dual_shadow_1(self):
         self.verify_shadow_types('LFEV', '1221', 'LFEV', 'LEFV', 'VEFL')
@@ -83,11 +95,11 @@ class ApShadowTypeCalculatorTest(unittest.TestCase):
     def test_dual_shadow_2(self):
         self.verify_shadow_types('LFEV', '4334', 'LFEV', 'LEFV', 'VEFL')
 
-    def test_conflictor_shadow(self):
-        self.verify_shadow_types('evlf', '4124', 'EVLF', 'ELVF', 'FLVE', 'LFVE')
+    def test_swap_with_original_aspect(self):
+        self.verify_shadow_types('EVLF', '4124', 'EVLF', 'ELVF', 'FLVE', 'VLFE')
 
     def test_swap_everything(self):
-        self.verify_shadow_types('FelV', '2312', 'FELV', 'FLEV', 'LFEV', 'EFLV', 'EVLF')
+        self.verify_shadow_types('LFVE', '2111', 'LFVE', 'EFVL', 'ELVF', 'FLVE', 'VLFE')
 
 
 if __name__ == '__main__':
