df4cb3db25eeabca059932530d53c96cbf88da25
Richard Bogart <trbogart@gmail.com>
2025-09-01 08:56:14 -0700

    More README

--- run_ap_discord.sh
+++ run_ap_discord.sh
@@ -1,11 +1,29 @@
 #! /bin/bash
 
+error() {
+  echo "$@" >&2
+  exit 1
+}
+
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-cd "$SCRIPT_DIR"
+# shellcheck disable=SC2164
+cd "$SCRIPT_DIR" || error "Unable to find script directory $SCRIPT_DIR"
 
 if [[ ! -d .venv/bin ]]; then
-  python3 -m venv .venv || exit 1
+  if command -v python3 >/dev/null 2>&1; then
+    python3 -m venv .venv || error "Unable to create venv"
+  elif command -v python >/dev/null 2>&1; then
+    python -m venv .venv || error "Unable to create venv"
+  else
+    error "Python not installed"
+  fi
 fi
-. .venv/bin/activate || exit 1
-pip install -q -r requirements.txt || exit 1
-python3 ap_discord.py | tee discord.log 2>&1
+. .venv/bin/activate || error "Unable to activate venv"
+pip install -q -r requirements.txt || error "Unable to install requirements"
+if command -v python3 >/dev/null 2>&1; then
+  python3 ap_discord.py | tee discord.log 2>&1
+elif command -v python >/dev/null 2>&1; then
+  python ap_discord.py | tee discord.log 2>&1
+else
+  error "Python not installed"
+fi
