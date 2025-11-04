2bc46b444f81d6d0f745747ffd97ad3539bdbad4
Richard Bogart <trbogart@gmail.com>
2025-11-04 11:28:34 -0800

    script

--- run_ap_discord.sh
+++ run_ap_discord.sh
@@ -1,34 +1,43 @@
 #! /bin/bash
 
+log() {
+  echo ------------------------------------------
+  echo "$@"
+}
 error() {
   echo "$@" >&2
   exit 1
 }
 
-SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-# shellcheck disable=SC2164
-cd "$SCRIPT_DIR" || error "Unable to find script directory $SCRIPT_DIR"
-
-if [[ ! -f .venv/bin/activate ]]; then
+run_python() {
+  error_msg=$1
+  shift
   if command -v python3 >/dev/null 2>&1; then
-    python3 -m venv .venv || error "Unable to create venv"
+    python3 "$@" || error "$error_msg"
   elif command -v python >/dev/null 2>&1; then
-    python -m venv .venv || error "Unable to create venv"
+    python "$@" || error "$error_msg"
   else
     error "Python not installed"
   fi
+}
+
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+SCRIPT=ap_discord.py
+cd "$SCRIPT_DIR" || error "Unable to find script directory $SCRIPT_DIR"
+
+if [[ ! -f .venv/bin/activate ]]; then
+  log "Creating venv"
+  run_python "Unable to create venv" -m venv .venv
 fi
-. .venv/bin/activate || error "Unable to activate venv"
-if command -v python3 >/dev/null 2>&1; then
-  python3 -m ensurepip --upgrade || error "Unable to install/upgrade pip"
-elif command -v python >/dev/null 2>&1; then
-  python -m ensurepip --upgrade || error "Unable to install/upgrade pip"
-fi
-pip install -q -r requirements.txt || error "Unable to install requirements"
-if command -v python3 >/dev/null 2>&1; then
-  python3 ap_discord.py | tee discord.log 2>&1
-elif command -v python >/dev/null 2>&1; then
-  python ap_discord.py | tee discord.log 2>&1
-else
-  error "Python not installed"
-fi
+
+log "Activating venv"
+source .venv/bin/activate || error "Unable to activate venv"
+
+log "Installing pip"
+run_python "Unable to install pip" -m ensurepip
+
+log "Upgrading dependencies"
+pip install -q -U pip -r requirements.txt --require-virtualenv || error "Unable to upgrade dependencies"
+
+log "Running $SCRIPT"
+run_python "Error running $SCRIPT" "$SCRIPT"
