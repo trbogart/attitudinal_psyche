352c1c0d900d19e0d48dc0ae670d1c3221273298
Richard Bogart <trbogart@gmail.com>
2025-11-06 22:37:06 -0800

    add stats script

---
+++ typing_stats.py
@@ -0,0 +1,86 @@
+# temporary script to calculate function/blocks that have been found with new typings
+
+# typing videos with function/block
+typing_videos = [
+    'FVLE', # Adriana Lima 9/8
+    'VFEL', # Kate Moss 9/15
+    'FLVE', # Bailey Sarian 9/22
+    'VLFE', # Azealia Banks 9/23
+    'FVEL', # Sofia Isella 9/29
+    'FVEL', # Adrianne Lenker 10/3
+    'VLFE', # Owen Cook 10/6
+    'LVFE', # Nick LS De Cesare 10/10
+    'VEFL', # Kobe Bryant 10/14
+    'FVEL', # Bjork 10/18
+    'EFVL', # HRH Collection 10/21
+    'VLFE', # Bo Burnham 10/27
+    'VELF', # Seal 11/3
+    'ELVF', # Asmongold 11/6 (theory)
+]
+
+class TypingStats:
+    # TODO keep track of counts and/or specific types
+    def __init__(self, print_derived = True):
+        self.print_derived = print_derived
+        self.type_set = set(typing_videos)
+        self.pair_count = 0
+        self.pair_count_directional = 0
+
+        self.missing_attitudes = {}
+        for pos in range(1,5):
+            for aspect in ['V', 'L', 'F', 'E']:
+                attitude = f'{pos}{aspect}'
+                self.missing_attitudes[attitude] = None
+        for typings in self.type_set:
+            for i, aspect in enumerate(typings):
+                attitude = f'{i+1}{aspect}'
+                if attitude in self.missing_attitudes:
+                    del self.missing_attitudes[attitude]
+
+        print('Missing attitudes:')
+        if len(self.missing_attitudes) == 0:
+            print('- None')
+        else:
+            for attitude in self.missing_attitudes.keys():
+                print(f'- {attitude}')
+        print()
+
+        # blocks
+        self.print_blocks('Strategist', 'V', 'L')
+        self.print_blocks('Experiencer', 'V', 'F')
+        self.print_blocks('Conceptualist', 'V', 'E')
+
+        # blocks that can be derived from the previous 3, so do not increment counts
+        self.print_blocks('Reactivist', 'F', 'E', derived=True)
+        self.print_blocks('Evaluator', 'L', 'E', derived=True)
+        self.print_blocks('Realist', 'L', 'F', derived=True)
+
+        print('Summary:')
+        print(f'- {len(typing_videos)} typing videos with functions and blocks')
+        print(f'- {len(self.type_set)} of 24 types')
+        print(f'- {16-len(self.missing_attitudes)} of 16 attitudes')
+        print(f'- {self.pair_count} of 18 unique function/block pairs')
+        print(f'- {self.pair_count_directional} of 36 unique function/block pairs (directional)')
+
+    def print_blocks(self, block, aspect1, aspect2, derived = False):
+        if not derived or self.print_derived:
+            print(f'Functions with {block} ({aspect1}+{aspect2})')
+            for i in range(4):
+                for j in range(i+1, 4):
+                    aspects = []
+                    if any(type[i] == aspect1 and type[j] == aspect2 for type in self.type_set):
+                        aspects.append(f'{aspect1}{aspect2}')
+                    if any(type[i] == aspect2 and type[j] == aspect1 for type in self.type_set):
+                        aspects.append(f'{aspect2}{aspect1}')
+                    if aspects:
+                        if not derived:
+                            self.pair_count = self.pair_count + 1
+                            self.pair_count_directional = self.pair_count_directional + len(aspects)
+                        aspects_string = ' and '.join(aspects)
+                    else:
+                        aspects_string = 'None'
+                    print(f'- {i+1}+{j+1} {block}: {aspects_string}')
+            print()
+
+if __name__ == '__main__':
+    TypingStats()
